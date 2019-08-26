b3cb71209ebae8c1aa08c413e9a32b9b67bf3463
fortyrunner <fortyrunner@gmail.com>
2019-08-26 13:04:52 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+function nRating(rating: string, rateBand: string, n: number):[string, boolean, string] {
+  const rn:string = rateBand.repeat(n);
+  if (rating.indexOf(rn) >= 0){
+    return [rn, true, rating];
+  }
+  return [rating, false, rating];
+}
+
+
+function adjustRating(rating: string, rateBand: string) : [string, string] {
+
+  if (rating.indexOf(rateBand) >= 0){
+
+    let [newRating, matched, originalRating] = nRating(rating, rateBand, 3);
+
+    if (matched){
+      return [newRating, rating]
+    }
+
+    [newRating, matched] = nRating(rating, rateBand, 2);
+    if (matched){
+      return [newRating, rating]
+    }
+
+    [newRating, matched] = nRating(rating, rateBand, 1);
+    if (matched){
+      return [newRating, rating]
+    }
+
+  }
+
+  console.info("No match")
+
+  return [rating, rating];
+}
+
+
+
+console.info(adjustRating("BBB*", "B"));
+console.info(adjustRating("BBB+", "B"));
+console.info(adjustRating("BB+NR", "B"));
+
+console.info(adjustRating("CCC+NR", "C"));
+
+console.info(adjustRating("", "B"));
